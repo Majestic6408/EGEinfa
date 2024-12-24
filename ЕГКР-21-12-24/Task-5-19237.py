3907f918be97ee102c00a773a42a2a6531fdd0aa
IIvanov <Muromets39@mail.ru>
2024-12-24 19:15:20 +0200

    Решение задания номер 5-19237

--- ЕГКР-21-12-24/Task-5-19237.py
+++ ЕГКР-21-12-24/Task-5-19237.py
@@ -1,3 +1,29 @@
-print('hello')
+#Шаблон до 9
+def convert(num, sys):
+    res = ''
+    while num:
+        res += str(num % sys)
+        num //= sys
+    return res[::-1]
 
-print('World')
+ans = []
+for N in range(1, 100_000):
+    R = convert(N, 3)
+    if N % 3 == 0:
+        R += R[-2:]
+    else:
+        R += convert(sum(map(int, R)), 3)
+    R = int(R, 3)
+    if R % 2 == 0 and R > 220:
+        ans.append(R)
+
+print(min(ans))
+#Принт R нельзя!!!
+
+
+
+
+
+
+
+
