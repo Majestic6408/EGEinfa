aaad962790519457602d6f270a6ee2fb732628b0
Majestic6408 <Muromets39@mail.ru>
2024-12-27 15:27:49 +0200

    задание 8-19240

--- ЕГКР-21-12-24/Task-8-19240.py
+++ ЕГКР-21-12-24/Task-8-19240.py
@@ -2,7 +2,7 @@ from itertools import product
 
 alph = sorted('ЯНВАРЬ')
 
-for pos, val in enumerate(product(aplh, repeat=5), 1):
+for pos, val in enumerate(product(alph, repeat=5), 1):
     val = ''.join(val)
     if val[0] != 'Я' and val.count('Ь') == 1 and 'ЯЯ' not in val:
         print(pos)
