1cfe73779baba197339b08cac25370e70d2d2435
Majestic6408 <Muromets39@mail.ru>
2025-01-07 19:52:58 +0200

    задание 8-13298

---
+++ ЕГЭ-07-01-25/Task-8-13298.py
@@ -0,0 +1,20 @@
+from itertools import product
+
+alph = sorted(set('ПРИВЫЧКА'))
+
+arr = []
+for i in product(alph, repeat=5):
+    arr.append(''.join(i))
+
+pos = 0
+for i in range(len(arr)):
+    if(i + 1) % 5 == 0:
+        continue
+    pos += 1
+    for j in 'ИЫА':
+        if j in arr[i]:
+            break
+    else:
+        if len(arr[i]) == len(set(arr[i])):
+            print(pos)
+            break
