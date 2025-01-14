73733201cc34578057fd743df108145f700ccd62
IIvanov <Muromets39@mail.ru>
2025-01-14 19:00:51 +0200

    задание 2-1846

---
+++ ЕГЭ-14-01-25/Task-2-1846.py
@@ -0,0 +1,7 @@
+for a in range(2):
+    for b in range(2):
+        for c in range(2):
+            for d in range(2):
+                F = (not a and not b) or (b == c) or d
+                if not F:
+                    print(a, b, c, d)
