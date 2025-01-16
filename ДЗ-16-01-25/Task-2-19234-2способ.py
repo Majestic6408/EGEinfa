91cbf46198dbbf9fc679b770c2a9cc0bd150d5cf
Majestic6408 <Muromets39@mail.ru>
2025-01-17 01:52:34 +0200

    задание 2-19234 2 способ

---
+++ ДЗ-16-01-25/Task-2-19234-2способ.py
@@ -0,0 +1,7 @@
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                F = not((not x or y ) and not w) or not(z and not(y and w))
+                if not F:
+                    print(x, y, z, w)
