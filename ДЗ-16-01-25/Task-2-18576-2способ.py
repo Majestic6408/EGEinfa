9be754e4c0e03869806e7e4f514bef9355ee5ad5
IIvanov <Muromets39@mail.ru>
2025-01-17 18:16:53 +0200

    задание 2-18576 2 способ

--- ДЗ-16-01-25/Task-2-18576-2способ.py
+++ ДЗ-16-01-25/Task-2-18576-2способ.py
@@ -2,6 +2,6 @@ for x in range(2):
     for y in range(2):
         for z in range(2):
             for w in range(2):
-                F = not(w <=(x == y or y)) and (z <= x)
+                F = not(w <=(x == (y or y))) and (z <= x)
                 if F:
                     print(x, y, z, w)
