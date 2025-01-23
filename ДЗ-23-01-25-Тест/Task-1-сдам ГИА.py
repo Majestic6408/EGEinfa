7768551e14aaa347d322446b6ebe322c8dc29a81
Majestic6408 <Muromets39@mail.ru>
2025-01-24 00:10:23 +0200

    задание 1 гиа ответ 41

---
+++ ДЗ-23-01-25-Тест/Task-1-сдам ГИА.py
@@ -0,0 +1,10 @@
+from itertools import permutations
+
+graph = 'BD BC BF FG CE CG GA AE DE'.split()
+
+matrix = '47 57 45 136 236 457 126'.split()
+
+print(*range(1, 8))
+for i in permutations('ABCDEFG'):
+    if all(str(i.index(x) + 1) in matrix[i.index(y)] for x, y in graph):
+        print(*i)
