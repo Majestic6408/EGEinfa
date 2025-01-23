7997b0a6b1b4afa597b2f9f9b040de7a6cea3619
Majestic6408 <Muromets39@mail.ru>
2025-01-24 01:11:07 +0200

    задание 6 гиа ответ 32

---
+++ ДЗ-23-01-25-Тест/Task-6-сдам ГИА.py
@@ -0,0 +1,45 @@
+from turtle import *
+
+lt(90)
+tracer(0)
+m = 8
+
+for i in range(9):
+    fd(15 * m)
+    rt(90)
+    fd(25 * m)
+    rt(90)
+
+up()
+
+bk(10 * m)
+rt(90)
+
+down()
+
+for i in range(8):
+    fd(15 * m)
+    lt(90)
+    fd(25 * m)
+    lt(90)
+
+up()
+
+fd(6 * m)
+lt(90)
+
+down()
+
+for i in range(7):
+    fd(15 * m)
+    rt(90)
+    fd(25 * m)
+    rt(90)
+
+up()
+for x in range(-30, 30):
+    for y in range(-30, 30):
+        goto(x * m, y * m)
+        dot(3, 'red')
+
+done()
