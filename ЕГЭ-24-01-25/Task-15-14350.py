441c2a982354d3f54d61dac7187184eb46ed4a56
IIvanov <Muromets39@mail.ru>
2025-01-24 20:00:37 +0200

    задание 15-14350

---
+++ ЕГЭ-24-01-25/Task-15-14350.py
@@ -0,0 +1,11 @@
+def f(A):
+    for x in range(1000):
+        for y in range(1000):
+            f = not((x < 7) or (y >= 3 * x + A - 20) or (x >= 34) or (y < 121))
+            if f:
+                return 0
+    return 1
+
+for A in range(1000):
+    if f(A):
+        print(A)
