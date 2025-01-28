5aaea2e50bcb6d2a6185c39605bc5efbda9b3eb8
IIvanov <Muromets39@mail.ru>
2025-01-28 18:58:06 +0200

    задание 6-14173

---
+++ ЕГЭ-28-01-25/Task-6-14173.py
@@ -0,0 +1,34 @@
+from turtle import *
+tracer(0)
+m = 13
+lt(90)
+
+for i in range(2):
+    down()
+    for i in range(2):
+        fd(8 * m)
+        rt(90)
+        fd(8 * m)
+        rt(90)
+    up()
+    fd(6 * m)
+    rt(90)
+    fd(6 * m)
+    lt(90)
+
+rt(180)
+fd(4 * m)
+down()
+
+for i in range(4):
+    fd(8 * m)
+    rt(270)
+
+up()
+
+for x in range(-30, 30):
+    for y in range(-30, 30):
+        goto(x * m, y * m)
+        dot(3, 'white')
+
+done()
