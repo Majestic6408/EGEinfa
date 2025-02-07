6a9e4e4e6ae4f17b689958f3136ed72f8d3350d1
IIvanov <Muromets39@mail.ru>
2025-02-07 18:12:20 +0200

    задание 15-5437 решенно

--- ДЗ-05-02-25/Task-15-5437.py
+++ ДЗ-05-02-25/Task-15-5437.py
@@ -1,8 +1,8 @@
 def f(A):
-    for x in range(1, 1000):
-        for y in range(1, 1000):
-            for z in range(1, 1000):
-                f = ((z % 115 != 0) or (y % 78 != 0) or (x % 51 != 0)) <= (x % A != 0)
+    for x in range(1, 200):
+        for y in range(1, 200):
+            for z in range(1, 200):
+                f = ((z % 115 == 0) or (y % 78 == 0) or (x % 51 == 0)) <= (x % A == 0)
                 if not f:
                     return 0
     return 1
