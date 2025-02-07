33cf7d040c8be536e6c083001ca6fa44a23ceafb
IIvanov <Muromets39@mail.ru>
2025-02-07 18:08:22 +0200

    задание 15-8677 решенно

--- ДЗ-05-02-25/Task-15-8677.py
+++ ДЗ-05-02-25/Task-15-8677.py
@@ -1,7 +1,7 @@
 def f(A):
     for x in range(1, 1000):
         B = 80 <= x <= 100
-        f = (x % 17 != 0) <= ((not B) or (A < x + 30))
+        f = (x % 17 == 0) <= ((not B) or (A < x + 30))
         if not f:
             return 0
     return 1
