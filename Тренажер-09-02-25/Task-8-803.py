931015181f67c53de4d2cb007fffd257d1d33844
Majestic6408 <Muromets39@mail.ru>
2025-02-09 21:29:46 +0200

    задание 8-803

---
+++ Тренажер-09-02-25/Task-8-803.py
@@ -0,0 +1,12 @@
+from itertools import product
+from string import digits, ascii_uppercase
+
+a = digits + ascii_uppercase
+a = a[:15]
+cnt = 0
+for val in product(a, repeat=5):
+    val = ''.join(val)
+    if val[0] != '0' and val.count('8') == 1 and val.count('A') + val.count('B') + val.count('C') + val.count('D') + val.count('E') + val.count('F') >= 2:
+        cnt += 1
+
+print(cnt)
