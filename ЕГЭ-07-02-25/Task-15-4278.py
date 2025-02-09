836fa346973eff34d74d93283747a0c2d1def3a7
Majestic6408 <Muromets39@mail.ru>
2025-02-09 16:33:37 +0200

    задание 15-4278 решенно

--- ЕГЭ-07-02-25/Task-15-4278.py
+++ ЕГЭ-07-02-25/Task-15-4278.py
@@ -1,13 +1,13 @@
 def f(A):
     for x in range(1, 1000):
-        for A in range(1, 1001):
-            f = ((A % 12 == 0) and (530 % x == 0)) <= ((not(A % x == 0)) <= (not(170 % x == 0)))
+            f = (A % 12 == 0) and ((530 % x == 0) <= ((A % x != 0) <= (170 % x != 0)))
             if not f:
                 return 0
     return 1
 
 cnt = 0
-if f == True:
-    cnt += 1
+for A in range(1, 1001):
+    if f(A):
+        cnt += 1
 
 print(cnt)
