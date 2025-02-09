1306cb2e370ba1bf0137813385178636c3279a8e
Majestic6408 <Muromets39@mail.ru>
2025-02-09 17:34:30 +0200

    задание 5-501 шаг в будущее

---
+++ Тренажер-09-02-25/Task-5-501.py
@@ -0,0 +1,21 @@
+def convert(num, sys):
+    res = ''
+    while num:
+        res += str(num % sys)
+        num //= sys
+    return res[::-1]
+
+ans = []
+for N in range(1, 1000):
+    R = convert(N, 3)
+    if N % 3 == 0:
+        R = R + R[-2:]
+    else:
+        summ = sum(map(int, R))
+        summ = convert(summ, 3)
+        R = R + summ
+    R = int(R, 3)
+    if R > 220 and R % 2 == 0:
+        ans.append(R)
+
+print(min(ans))
