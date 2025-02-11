9138a00f7ff14294b893a191619b11acd208decd
IIvanov <Muromets39@mail.ru>
2025-02-11 19:47:11 +0200

    задание 17-9748

---
+++ ЕГЭ-11-02-25/Task-17-9748.py
@@ -0,0 +1,21 @@
+with open('17_9748.txt') as file:
+    data = [int(i) for i in file]
+
+maxx = max(i for i in data if str(i)[-2:] == '15')
+ans = []
+for i in range(len(data) - 2):
+    num1, num2, num3 = data[i], data[i + 1], data[i + 2]
+    summ = num1 + num2 + num3
+
+    u1 = len(str(num1)) == 4
+    u2 = len(str(num2)) == 4
+    u3 = len(str(num3)) == 4
+
+    f1 = u1 + u2 + u3 == 1
+    f2 = summ >= maxx
+
+    if f1 and f2:
+        ans.append(summ)
+
+print(len(ans), max(ans))
+
