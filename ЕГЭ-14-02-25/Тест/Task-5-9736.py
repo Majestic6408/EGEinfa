f15d860bb525d747f69c8bb7afafc248361546c9
Majestic6408 <Muromets39@mail.ru>
2025-02-14 19:04:50 +0200

    задание 5-9736 тест

---
+++ ЕГЭ-14-02-25/Тест/Task-5-9736.py
@@ -0,0 +1,21 @@
+def convert(num, sys):
+    res = ''
+    while num:
+        res += str(num % sys)
+        num //= sys
+    return res[::-1]
+
+ans = []
+for N in range(1, 10000):
+    R = convert(N, 2)
+    if N % 3:
+        R = R + R[-3:]
+    if N % 3 != 0:
+        ostat = N % 3
+        ostat = int(ostat) * 3
+        ostat = convert(ostat, 2)
+        R = R + ostat
+    if int(R, 2) < 170:
+        ans.append(int(R, 2))
+
+print(max(ans))
