39b484460dc79f29dbd17c6112f46f0c04af6475
IIvanov <Muromets39@mail.ru>
2025-02-25 19:06:27 +0200

    задание 25-1867

---
+++ ЕГЭ-25-02-25/Task-25-1867.py
@@ -0,0 +1,20 @@
+def f(num):
+    res = set()
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            res |= {i, num // i}
+    res = sorted(res)
+
+    for i in res:
+        if str(i)[-1:] == '8' and i != 8:
+            return i
+    return 0
+
+cnt = 0
+for i in range(500001, 10000000):
+    res = f(i)
+    if res:
+        print(i, res)
+        cnt += 1
+        if cnt == 5:
+            break
