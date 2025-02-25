1aa5abe50c87e4a62bbb1102805aa50c6d2cc177
IIvanov <Muromets39@mail.ru>
2025-02-25 19:18:25 +0200

    задание 25-17879

---
+++ ЕГЭ-25-02-25/Task-25-17879.py
@@ -0,0 +1,21 @@
+def f(num):
+    res = set()
+    for i in range(2, int(num** 0.5) + 1):
+        if num % i == 0:
+            res |= {i, num // i}
+    res = sorted(res)
+
+    if len(res) >= 2:
+        M = max(res) + min(res)
+        if str(M)[-1:] == '4':
+            return M
+    return 0
+
+cnt = 0
+for i in range(800001, 10000000):
+    res = f(i)
+    if res:
+        print(i, res)
+        cnt += 1
+        if cnt == 5:
+            break
