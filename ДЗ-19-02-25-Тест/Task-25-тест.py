385fe37a2f7a8dcb769e64db1b65fa719951b82f
Majestic6408 <Muromets39@mail.ru>
2025-02-28 04:27:40 +0200

    задание 25-тест-неокончательный ответ

--- ДЗ-19-02-25-Тест/Task-25-тест.py
+++ ДЗ-19-02-25-Тест/Task-25-тест.py
@@ -1,6 +1,22 @@
 from fnmatch import fnmatch
 
-for i in range(7777, 10 ** 10, 7777):
+for i in range(125670 - 125670 % 7777, 10 ** 10, 7777):
     if fnmatch(str(i), '12*567?'):
         if i % 7777 == 0:
             print(i, i // 7777)
+from itertools import product
+
+ans = []
+for r in range(6):
+    for z in product('0123456789', repeat=r):
+        z = ''.join(z)
+        for v in '0123456789':
+
+            num = int(f'12{z}567{v}')
+
+            if num <= 10 ** 10 and num % 7777 == 0:
+                ans.append([num, num // 7777])
+
+ans = set(sorted(ans))
+for i in ans:
+    print(*i)
