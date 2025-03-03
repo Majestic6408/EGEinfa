c263d5ae7dbc9de92a3bf6c2b95467cec79e189f
Majestic6408 <Muromets39@mail.ru>
2025-03-04 01:11:03 +0200

    задание 8-15320 тест

---
+++ КЕГЭ/ТЕСТЫ/ДОСРОЧНАЯ ВОЛНА/Task-8-15320.py
@@ -0,0 +1,8 @@
+from itertools import product
+
+a = sorted(set('ПАРУС'))
+
+for pos, val in enumerate(product(a, repeat=5), 1):
+    val = ''.join(val)
+    if val.count('У') <= 1 and 'АА' not in val:
+        print(pos)
