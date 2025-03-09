653bb02f991a466cec47eed37db38e8f6db5e714
Majestic6408 <Muromets39@mail.ru>
2025-03-09 23:43:57 +0200

    задание 19-21-17532 тест

---
+++ КЕГЭ/ТЕСТЫ/ОСНОВНАЯ ВОЛНА 07-06-24/Task-19-21-17532.py
@@ -0,0 +1,12 @@
+def f(x, y, s):
+    if x + y >= 65:
+        return s % 2 == 0
+    if s == 0:
+        return 0
+    h = [f(x + 1, y, s - 1), f(x, y + 1, s - 1), f(x * 3, y, s - 1), f(x, y * 3, s - 1)]
+    return any(h) if (s - 1) % 2 == 0 else all(h)
+
+s1 = 6
+print([s for s in range(1, 59) if f(s1, s, 2)])
+print([s for s in range(1, 59) if not f(s1, s, 1) and f(s1, s, 3)])
+print([s for s in range(1, 59) if not f(s1, s, 2) and f(s1, s, 4)])
