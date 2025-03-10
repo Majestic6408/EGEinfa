19867e00fe44cffb3f4ad25d633f828dd0b45545
Majestic6408 <Muromets39@mail.ru>
2025-03-10 23:19:24 +0200

    задание 5-17546 тест

---
+++ КЕГЭ/ТЕСТЫ/ОСНОВНАЯ ВОЛНА 08-06-24/Task-5-17546.py
@@ -0,0 +1,17 @@
+def convert(num, sys):
+    res = ''
+    while num:
+        res += str(num % sys)
+        num //= sys
+    return res[::-1]
+ans = []
+for N in range(1, 12):
+    R = convert(N, 2)
+    if N % 2 == 0:
+        R = '10' + R
+    else:
+        R = '1' + R + '01'
+    R = int(R, 2)
+    ans.append(R)
+
+print(max(ans))
