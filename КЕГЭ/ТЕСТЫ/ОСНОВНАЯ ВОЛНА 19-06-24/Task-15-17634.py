764ec050a5389e41fd9bce63814dbdf777f3950a
Majestic6408 <Muromets39@mail.ru>
2025-03-11 15:06:42 +0200

    задание 15-17634 тест

---
+++ КЕГЭ/ТЕСТЫ/ОСНОВНАЯ ВОЛНА 19-06-24/Task-15-17634.py
@@ -0,0 +1,11 @@
+def f(A):
+    for x in range(1, 1000):
+        for y in range(1, 1000):
+            f = (x + y <= 30) or (y <= x + 2) or (y >= A)
+            if not f:
+                return 0
+    return 1
+
+for A in range(1, 10000):
+    if f(A):
+        print(A)
