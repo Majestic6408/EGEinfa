7e617a0ff6d0c93f6a0c768d4193d6a59721feab
Majestic6408 <Muromets39@mail.ru>
2025-03-11 15:05:31 +0200

    задание 8-17627 тест не решено

---
+++ КЕГЭ/ТЕСТЫ/ОСНОВНАЯ ВОЛНА 19-06-24/Task-8-17627.py
@@ -0,0 +1,10 @@
+# from string import ascii_uppercase, digits
+# from itertools import product
+# a = ascii_uppercase + digits
+#
+# a = a[:15]
+#
+# for val in product(a, repeat=5):
+#     val = ''.join(val)
+#     if val[0] != '0' and val.count('8') == 1:
+#         chisl = a[10:15]
