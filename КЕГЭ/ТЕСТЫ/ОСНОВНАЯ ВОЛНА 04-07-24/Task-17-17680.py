7e241bbad219ebea0fc659f346637edc76709477
Majestic6408 <Muromets39@mail.ru>
2025-03-16 18:20:32 +0200

    задание 17-17680

---
+++ КЕГЭ/ТЕСТЫ/ОСНОВНАЯ ВОЛНА 04-07-24/Task-17-17680.py
@@ -0,0 +1,14 @@
+with open('17_17680.txt') as file:
+    data = [int(i) for i in file]
+
+minn = min(i for i in data if i > 0 and i % 41 == 0)
+ans = []
+for num1, num2 in zip(data, data[1:]):
+    summ = num1 + num2
+
+    f1 = num1 != num2
+    u1 = abs(num1 - num2) % minn == 0
+    if f1 and u1:
+        ans.append(summ)
+
+print(len(ans), max(ans))
