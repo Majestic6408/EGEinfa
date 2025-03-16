deafda65a928d74587642e9a9c295196dd6413bb
Majestic6408 <Muromets39@mail.ru>
2025-03-16 18:19:11 +0200

    задание 19-21-17613

---
+++ КЕГЭ/ТЕСТЫ/ОСНОВНАЯ ВОЛНА 19-06-24 СИБИРЬ/Task-19-21-17613.py
@@ -0,0 +1,13 @@
+def f(x, y, s):
+    if x + y >= 227:
+        return s % 2 == 0
+    if s == 0:
+        return 0
+    h = [f(x + 1, y, s - 1), f(x, y + 1, s - 1), f(x * 2, y, s - 1), f(x, y * 2, s - 1)]
+    return any(h) if (s - 1) % 2 == 0 else all(h)
+
+s1 = 17
+
+print([s for s in range(1, 210) if f(s, s1, 2)])
+print([s for s in range(1, 210) if not f(s, s1, 1) and f(s, s1, 3)])
+print([s for s in range(1, 210) if not f(s, s1, 2) and f(s, s1, 4)])
