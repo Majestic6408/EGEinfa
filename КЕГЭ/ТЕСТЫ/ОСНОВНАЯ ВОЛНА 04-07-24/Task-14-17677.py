d9f8f5a38ccd7b3a80c0ae373ff53d08cda43312
Majestic6408 <Muromets39@mail.ru>
2025-03-16 18:19:53 +0200

    задание 14-17677

---
+++ КЕГЭ/ТЕСТЫ/ОСНОВНАЯ ВОЛНА 04-07-24/Task-14-17677.py
@@ -0,0 +1,15 @@
+def convert(num, sys):
+    res = ''
+    while num:
+        res += str(num % sys)
+        num //= sys
+    return res[::-1]
+ans = []
+for x in range(1, 2031):
+    num = 6 ** 260 + 6 ** 160 + 6 ** 60 - x
+    num = convert(num, 6)
+    ans.append([num.count('0'), x])
+
+ans = sorted(ans)
+
+print(ans)
