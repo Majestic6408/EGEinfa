ca36979562c2ac7c74ef2df7d2605230b6cf8f5b
Majestic6408 <Muromets39@mail.ru>
2025-03-18 15:01:25 +0200

    задание 5-19237 тест

---
+++ КЕГЭ/ТЕСТЫ/ЕГКР-21-12-24-2/Task-5-19237.py
@@ -0,0 +1,20 @@
+def convert(num, sys):
+    res = ''
+    while num:
+        res += str(num % sys)
+        num //= sys
+    return res[::-1]
+ans = []
+for N in range(1, 100000):
+    R = convert(N, 3)
+    if N % 3 == 0:
+        R = R + R[-2:]
+    else:
+        summ = sum(map(int, R))
+        summ = convert(summ, 3)
+        R = R + summ
+    R = int(R, 3)
+    if R > 220:
+        ans.append(R)
+
+print(min(ans))
