1a3c0a6bff47bf93b7ec1b821501b1274430f2e0
IIvanov <Muromets39@mail.ru>
2025-03-18 20:18:55 +0200

    задание 23-16332 тест

---
+++ ЕГЭ-18-03-25-Тест/Task-23-16332.py
@@ -0,0 +1,8 @@
+def f(st, end):
+    if st >end:
+        return 0
+    if st == end:
+        return 1
+    return f(st + 1,end) + f(st + 2, end) + f(st * 2, end)
+
+print(f(4, 11) * f(11, 13) * f(13, 15))
