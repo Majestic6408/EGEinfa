b7c7f6af4a7cd9ed8c314b1dd05d5f85f47342ab
IIvanov <Muromets39@mail.ru>
2025-03-28 19:38:23 +0200

    задание 19-21-4599

--- ЕГЭ-28-03-25/Task-19-21-4599.py
+++ ЕГЭ-28-03-25/Task-19-21-4599.py
@@ -4,7 +4,7 @@ def f(x, y, s):
     if s == 0:
         return 0
     h = [f(x + 1, y, s - 1), f(x, y + 1, s - 1), f(x * 2, y, s - 1), f(x, y * 2, s - 1)]
-    return any(h) if (s - 1) % 2 == 0 else all  (h)
+    return any(h) if (s - 1) % 2 == 0 else all(h)
 
 s1 = 17
 print([s for s in range(1, 242) if f(s, s1, 2)])
