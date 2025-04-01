10f6266d754767d099893af279b6d9946b23f73d
IIvanov <Muromets39@mail.ru>
2025-04-01 19:50:30 +0200

    задание 19-21-1374 гроб (не решен:( )

--- КЕГЭ/ЗАДАНИЯ/ЗАДАНИЕ-19-21/Task-19-21-1374.py
+++ КЕГЭ/ЗАДАНИЯ/ЗАДАНИЕ-19-21/Task-19-21-1374.py
@@ -1,6 +1,8 @@
 def f(x, y, s):
-    if 107 <= (x + y) <= 170:
+    if 107 <= x + y <= 170:
         return s % 2 == 0
+    if x + y > 170:
+        return s % 2 != 0
     if s == 0:
         return 0
     h = [f(x + 10, y, s - 1), f(x, y + 10, s - 1), f(x * 2, y, s - 1), f(x, y * 2, s - 1)]
@@ -8,5 +10,5 @@ def f(x, y, s):
 
 s1 = 5
 print([s for s in range(1, 101) if f(s, s1, 2)])
-print([s for s in range(1, 101) if f(s, s1, 1) or f(s, s1, 3) and not f(s, s1, 2) and f(s, s1, 3)])
+print([s for s in range(1, 101) if not f(s, s1, 1) and not f(s, s1, 2) and f(s, s1, 3)])
 print([s for s in range(1, 101) if f(s, s1, 2) or f(s, s1, 4) and not f(s, s1, 2)])
