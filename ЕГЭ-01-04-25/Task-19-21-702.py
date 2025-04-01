3ba225216ad49a4f8f4c8bd731423955f0f4ecf6
IIvanov <Muromets39@mail.ru>
2025-04-01 19:50:58 +0200

    задание 19-21-702

---
+++ ЕГЭ-01-04-25/Task-19-21-702.py
@@ -0,0 +1,13 @@
+def f(x, s):
+    if 36 <= x <= 85:
+        return s % 2 == 0
+    if x > 85:
+        return s % 2 !=0
+    if s == 0:
+        return 0
+    h = [f(x + 2, s - 1), f(x * 3, s - 1)]
+    return any(h) if (s - 1) % 2 == 0 else all(h)
+
+print([s for s in range(1, 36) if f(s, 2)])
+print([s for s in range(1, 36) if f(s, 5)])
+print([s for s in range(1, 36) if f(s, 6)])
