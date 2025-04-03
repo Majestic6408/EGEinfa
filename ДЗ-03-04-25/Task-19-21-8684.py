fcdaaeff7d00ef20f4cac8e7d501b605451ccf2f
Majestic6408 <Muromets39@mail.ru>
2025-04-03 22:43:53 +0200

    задание 19-21-8684

---
+++ ДЗ-03-04-25/Task-19-21-8684.py
@@ -0,0 +1,12 @@
+def f(x, y, s):
+    if x + y >= 175:
+        return s % 2 == 0
+    if s == 0:
+        return 0
+    h = [f(x + 1, y, s - 1), f(x, y + 1, s - 1), f(x * 3, y, s - 1), f(x, y * 3, s - 1)]
+    return any(h) if (s - 1) % 2 == 0 else all(h)
+
+s1 = 19
+print([s for s in range(1, 154) if f(s, s1, 2)])
+print([s for s in range(1, 154) if not f(s, s1,1) and f(s, s1,3)])
+print([s for s in range(1, 154) if not f(s, s1,2) and f(s, s1,4)])
