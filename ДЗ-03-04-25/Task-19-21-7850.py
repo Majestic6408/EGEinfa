62b6cebbd1890ad190242dbc1c4159bf4aaad8ec
Majestic6408 <Muromets39@mail.ru>
2025-04-03 22:43:44 +0200

    задание 19-21-7850

---
+++ ДЗ-03-04-25/Task-19-21-7850.py
@@ -0,0 +1,12 @@
+def f(x, s):
+    if x >= 73:
+        return s % 2 == 0
+    if s == 0:
+        return 0
+    h = [f(x + 1, s - 1), f(x + 3, s - 1), f(x * 2, s - 1)]
+    return any(h) if (s - 1) % 2 == 0 else all(h)
+
+
+print([s for s in range(1, 73) if f(s, 2)])
+print([s for s in range(1, 73) if not f(s, 1) and f(s, 3)])
+print([s for s in range(1, 73) if not f(s, 2) and f(s, 4)])
