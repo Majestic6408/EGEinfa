42a389cf7d58581b58fa4c08b86f4aecbda5c416
Majestic6408 <Muromets39@mail.ru>
2025-04-15 18:25:39 +0200

    задание 17-12249

---
+++ ЕГЭ-15-04-25/Task-17-12249.py
@@ -0,0 +1,17 @@
+with open ('17_12249.txt') as file:
+    data = [int(i) for i in file]
+
+maxx = max(i for i in data if len(str(abs(i))) == 5 and str(i)[-1:] == '3')
+ans = []
+for num1, num2, num3 in zip(data, data[1:], data[2:]):
+    summ = num1 + num2 + num3
+
+    u1 = str(num1)[-1:] == '3'
+    u2 = str(num2)[-1:] == '3'
+    u3 = str(num3)[-1:] == '3'
+    f1 = u1 + u2 + u3 >= 1
+    f2 = summ <= maxx
+    if f1 and f2:
+        ans.append(summ)
+
+print(len(ans), max(ans))
