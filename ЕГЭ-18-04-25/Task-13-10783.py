94c60b3f6735d6ef9aef109f3c51c1e712acee2e
Majestic6408 <Muromets39@mail.ru>
2025-05-02 18:03:53 +0200

    задание 13-10783

---
+++ ЕГЭ-18-04-25/Task-13-10783.py
@@ -0,0 +1,12 @@
+from ipaddress import ip_address, ip_network
+
+ip1 = ip_address('121.171.5.70')
+ip2 = ip_address('121.171.5.107')
+
+for mask in range(33):
+    net1 = ip_network(f'{ip1}/{mask}', 0)
+    net2 = ip_network(f'{ip2}/{mask}', 0)
+    if net1.network_address == net2.network_address:
+        if ip1 not in (net1.network_address, net1.broadcast_address):
+            if ip2 not in (net2.network_address, net2.broadcast_address):
+                print(net1.num_addresses)
