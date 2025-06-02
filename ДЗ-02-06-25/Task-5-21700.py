ae460bd0c7899cf9bf1fce1a8debeb632d9aeb2c
Majestic6408 <Muromets39@mail.ru>
2025-06-03 00:01:14 +0200

    Задание 5-21700

---
+++ ДЗ-02-06-25/Task-5-21700.py
@@ -0,0 +1,21 @@
+def convert(num, sys):
+    res = ''
+    while num:
+        res += str(num % sys)
+        num //= sys
+    return res[::-1]
+
+ans = []
+for N in range(1, 1000):
+    R = convert(N, 3)
+    if N % 3 == 0:
+        R = R + R[-2:]
+    else:
+        ostat = (N % 3) * 3
+        ostat1 = convert(ostat, 3)
+        R = R + str(ostat1)
+    R = int(R, 3)
+    if R < 150:
+        ans.append(N)
+
+print(max(ans))
