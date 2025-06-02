dd7d744fec67366d2d9e8c7a90d40704db45c087
Majestic6408 <Muromets39@mail.ru>
2025-06-03 00:01:07 +0200

    Задание 5-21404

---
+++ ДЗ-02-06-25/Task-5-21404.py
@@ -0,0 +1,19 @@
+def convert(num, sys):
+    res = ''
+    while num:
+        res += str(num % sys)
+        num //= sys
+    return res[::-1]
+
+ans = []
+for N in range(1, 1000):
+    R = convert(N, 2)
+    if sum(map(int, R)) % 2 == 0:
+        R = '10' + R[2:] + '0'
+    else:
+        R = '11' + R[2:] + '1'
+    R = int(R, 2)
+    if R > 480:
+        ans.append(N)
+
+print(min(ans))
