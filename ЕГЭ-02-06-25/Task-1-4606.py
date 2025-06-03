d3ba59ce95fbad321a853d3a5b3390a970a75d75
IIvanov <Muromets39@mail.ru>
2025-06-03 12:42:46 +0200

    Задание

--- ЕГЭ-02-06-25/Task-1-4606.py
+++
@@ -1,9 +0,0 @@
-from itertools import permutations
-
-matrix = '68 47 45 237 368 15 248 157'.split()
-graph = 'AF FB AB FG GE EC CA CD DH HE'.split()
-
-print(*range(1, 9)) #количество столбцов в матрице
-for i in permutations('ABCDEFGH'):
-   if all(str(i.index(x) + 1) in matrix[i.index(y)] for x, y in graph):
-       print(*i)
