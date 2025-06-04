2e4bf7234f7b6d2456a7c2af796b54217ec44eb1
IIvanov <Muromets39@mail.ru>
2025-06-04 13:13:14 +0200

    Задание 12-9835

---
+++ Интенсив неделя/ЕГЭ-04-06-25/Task-12-9835.py
@@ -0,0 +1,12 @@
+ans = []
+for n in range(4, 10000):
+    st = '1' + '8' * n
+    while '18' in st or '388' in st or '888' in st:
+        st = st.replace('18', '8', 1)
+        st = st.replace('388', '81', 1)
+        st = st.replace('888', '3', 1)
+    if st.count('1') == 3:
+        ans.append(n)
+
+
+print(min(ans))
