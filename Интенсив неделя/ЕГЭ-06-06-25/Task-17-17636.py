e9396cd38b9591df988b2c97c1314c83bf68ce58
Majestic6408 <Muromets39@mail.ru>
2025-06-07 11:35:21 +0200

    Задание 17-17636

---
+++ Интенсив неделя/ЕГЭ-06-06-25/Task-17-17636.py
@@ -0,0 +1,18 @@
+with open('17_17636.txt') as file:
+    data = [int(i) for i in file]
+
+maxx = max(i for i in data if str(i)[-1] == '3' and len(str(abs(i))) == 3)
+
+ans = []
+for num1, num2, num3 in zip(data, data[1:], data[2:]):
+    summ = num1 + num2 + num3
+
+    u1 = str(num1)[-1] == '3' and len(str(abs(num1))) == 3
+    u2 = str(num2)[-1] == '3' and len(str(abs(num2))) == 3
+    u3 = str(num3)[-1] == '3' and len(str(abs(num3))) == 3
+    f1 = u1 + u2 + u3 >= 1
+    f2 = summ < maxx
+    if f1 and f2:
+        ans.append(summ)
+
+print(len(ans), max(ans))
