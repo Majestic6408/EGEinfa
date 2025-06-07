14440f0a6901fd664fd855662c98341fceee9fb2
IIvanov <Muromets39@mail.ru>
2025-06-07 13:55:38 +0200

    Задание 27-17882

---
+++ Интенсив неделя/ЕГЭ-07-06-25/Task-27-17882.py
@@ -0,0 +1,46 @@
+from math import dist
+
+def centroid(cluster):
+    distance = []
+    for dot in cluster:
+        sum_dist = 0
+        for dot2 in cluster:
+            sum_dist += dist(dot, dot2)
+        distance.append([sum_dist, dot])
+    return min(distance)[1]
+
+
+with open('27_A_17882.txt') as file:
+    cluster_A1 = []
+    cluster_A2 = []
+    for i in file:
+        x, y = map(float, i.split())
+        if y < 3:
+            cluster_A1.append([x, y])
+        else:
+            cluster_A2.append([x, y])
+
+clusters = [cluster_A1, cluster_A2]
+centers = [centroid(cluster) for cluster in clusters]
+px = sum(center[0] for center in centers) / len(centers)
+py = sum(center[1] for center in centers) / len(centers)
+print(int(px * 10000), int(py * 10000))
+
+with open('27_B_17882.txt') as file:
+    cluster_B1 = []
+    cluster_B2 = []
+    cluster_B3 = []
+    for i in file:
+        x, y = map(float, i.split())
+        if y > 4 and x > 5:
+            cluster_B1.append([x, y])
+        if y > 4 and x < 5:
+            cluster_B2.append([x, y])
+        if y < 4 and x < 5:
+            cluster_B3.append([x, y])
+
+clusters = [cluster_B1, cluster_B2, cluster_B3]
+centers = [centroid(cluster) for cluster in clusters]
+px = sum(center[0] for center in centers) / len(centers)
+py = sum(center[1] for center in centers) / len(centers)
+print(int(px * 10000), int(py * 10000))
