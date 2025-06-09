edd49c98fc4bbfdfc521bdbbc39bc3d3dbcd29d0
Majestic6408 <Muromets39@mail.ru>
2025-06-09 22:53:24 +0200

    Задание 6-21892 тест

---
+++ КЕГЭ/ТЕСТЫ/ОТКРЫТЫЙ ВАРИАНТ 2025/Task-6-21892.py
@@ -0,0 +1,21 @@
+from turtle import *
+
+m = 30
+tracer(0)
+screensize(4000, 4000)
+lt(90)
+
+rt(90)
+for i in range(7):
+    rt(45)
+    fd(11 * m)
+    rt(45)
+
+up()
+
+for x in range(-50, 50):
+    for y in range(-50, 50):
+        goto(x * m, y * m)
+        dot(4, 'red')
+
+done()
